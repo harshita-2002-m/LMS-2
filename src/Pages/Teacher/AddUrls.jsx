099f28837c16c0ab9dc30f9fc53ffd77c586bdd4
Harshita Makode <96257348+harshita-2002-m@users.noreply.github.com>
2023-12-01 12:18:16 +0530

    Update AddUrls.jsx

--- src/Pages/Teacher/AddUrls.jsx
+++ src/Pages/Teacher/AddUrls.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-
+ 
 const baseUrl = "https://danville.pythonanywhere.com/api";
-
+ 
 function AddUrls() {
   const [title, setTitle] = useState("");
   const [url, setUrl] = useState("");
   const { id } = useParams();
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-
+ 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+ 
     try {
       // Create an object with the data to send to the backend
       const data = {
@@ -21,14 +21,14 @@ function AddUrls() {
         urls: url,
         fk_course: id,
       };
-
+ 
       // Make a POST request to the backend endpoint
       await axios.post(`${baseUrl}/urls/`, data);
-
+ 
       // Clear the form
       setTitle("");
       setUrl("");
-
+ 
       setSuccessMessage("URL added successfully");
       setErrorMessage(""); // Clear any previous error message
     } catch (error) {
@@ -37,15 +37,15 @@ function AddUrls() {
       setSuccessMessage(""); // Clear any previous success message
     }
   };
-
+ 
   return (
     <div className="formcontainer videoQuizContainer">
       <form id="courseForm" onSubmit={handleSubmit}>
         <h3>ADD NEW URLs</h3>
-
+ 
         {successMessage && <p className="success-message">{successMessage}</p>}
         {errorMessage && <p className="error-message">{errorMessage}</p>}
-
+ 
         <div className="form-group">
           <label htmlFor="UrlsTitle">Title</label>
           <input
@@ -57,7 +57,7 @@ function AddUrls() {
             required
           />
         </div>
-
+ 
         <div className="form-group">
           <label htmlFor="urls">URL</label>
           <br />
@@ -70,7 +70,7 @@ function AddUrls() {
             required
           />
         </div>
-
+ 
         <div className="form-group">
           <button type="submit">Submit</button>
         </div>
@@ -78,21 +78,21 @@ function AddUrls() {
     </div>
   );
 }
-
+ 
 export default AddUrls;
-
+ 
 //const baseUrl = 'http://127.0.0.1:8000/api';
-
+ 
 // function AddUrls() {
-
+ 
 //     return (
 //         <div class="formcontainer videoQuizContainer">
 //           <form id="courseForm" action="#" method="POST">
 //             <h3>ADD NEW URLs</h3>
-
+ 
 //             <div class="form-group">
 //               <label for="courseName">Title</label>
-
+ 
 //               <input
 //                 type="text"
 //                 id="courseName"
@@ -101,12 +101,12 @@ export default AddUrls;
 //                 required
 //               />
 //             </div>
-
+ 
 //             <div className="form-group">
 //           <label htmlFor="video">URL:</label><br />
 //           <input type="file" id="video" name="video" required />
 //         </div>
-
+ 
 //             <div class="form-group">
 //               <button type="submit">Submit</button>
 //             </div>
