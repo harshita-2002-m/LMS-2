b2f622adc148a5c50285f625d086e7848751ebb4
Harshita Makode <96257348+harshita-2002-m@users.noreply.github.com>
2023-12-01 13:33:10 +0530

    Update OngoingCourse.jsx

--- src/Pages/Student/OngoingCourse.jsx
+++ src/Pages/Student/OngoingCourse.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "./Card";
-
+ 
 const baseUrl = "https://danville.pythonanywhere.com/api";
-
+ 
 function OngoingCourse() {
   const [ongoingCourses, setOngoingCourses] = useState([]);
   const [storedStudentId, setStoredStudentId] = useState(null);
-
+ 
   useEffect(() => {
     const fetchStudentId = () => {
       const storedId = localStorage.getItem("studentId");
@@ -15,10 +15,10 @@ function OngoingCourse() {
       // Parse to integer if storedId is expected to be a number
       setStoredStudentId(parseInt(storedId, 10));
     };
-
+ 
     fetchStudentId();
   }, []);
-
+ 
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -27,11 +27,11 @@ function OngoingCourse() {
         const enrolledCoursesData = enrollmentResponse.data
           .filter((enrollment) => enrollment.fk_student === storedStudentId)
           .map((enrollment) => enrollment.fk_course);
-
+ 
         // Fetch all courses
         const coursesResponse = await axios.get(baseUrl + "/course");
         const allCourses = coursesResponse.data;
-
+ 
         // Filter ongoing courses based on enrollment and end date
         const currentDate = new Date();
         const ongoing = allCourses.filter(
@@ -40,18 +40,18 @@ function OngoingCourse() {
             currentDate >= new Date(course.startDate) &&
             currentDate <= new Date(course.endDate)
         );
-
+ 
         setOngoingCourses(ongoing);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-
+ 
     if (storedStudentId) {
       fetchData();
     }
   }, [storedStudentId]);
-
+ 
   return (
     <div>
       <h2>Ongoing Courses</h2>
@@ -69,5 +69,5 @@ function OngoingCourse() {
     </div>
   );
 }
-
+ 
 export default OngoingCourse;
