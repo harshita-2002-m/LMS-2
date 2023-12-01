48ebb0b7b8f69b84f062e5ab7ae6f09ad6e1ab1d
Harshita Makode <96257348+harshita-2002-m@users.noreply.github.com>
2023-12-01 13:21:50 +0530

    Update CompletedCourse.jsx

--- src/Pages/Student/CompletedCourse.jsx
+++ src/Pages/Student/CompletedCourse.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "./Card";
-
+ 
 const baseUrl = "https://danville.pythonanywhere.com/api";
-
+ 
 function CompletedCourse() {
   const [completedCourses, setCompletedCourses] = useState([]);
   const [storedStudentId, setStoredStudentId] = useState(null);
-
+ 
   useEffect(() => {
     const fetchStudentId = () => {
       const storedId = localStorage.getItem("studentId");
@@ -15,10 +15,10 @@ function CompletedCourse() {
       // Parse to integer if storedId is expected to be a number
       setStoredStudentId(parseInt(storedId, 10));
     };
-
+ 
     fetchStudentId();
   }, []);
-
+ 
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -27,11 +27,11 @@ function CompletedCourse() {
         const enrolledCoursesData = enrollmentResponse.data
           .filter((enrollment) => enrollment.fk_student === storedStudentId)
           .map((enrollment) => enrollment.fk_course);
-
+ 
         // Fetch all courses
         const coursesResponse = await axios.get(baseUrl + "/course");
         const allCourses = coursesResponse.data;
-
+ 
         // Filter completed courses based on enrollment and end date
         const currentDate = new Date();
         const completed = allCourses.filter(
@@ -39,18 +39,18 @@ function CompletedCourse() {
             enrolledCoursesData.includes(course.id) &&
             currentDate >= new Date(course.endDate)
         );
-
+ 
         setCompletedCourses(completed);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-
+ 
     if (storedStudentId) {
       fetchData();
     }
   }, [storedStudentId]);
-
+ 
   return (
     <div>
       <h2>Completed Courses</h2>
@@ -68,5 +68,5 @@ function CompletedCourse() {
     </div>
   );
 }
-
+ 
 export default CompletedCourse;
