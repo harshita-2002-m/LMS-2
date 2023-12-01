7650a3851f67f5eb76296647b278bd0041529c06
Harshita Makode <96257348+harshita-2002-m@users.noreply.github.com>
2023-12-01 13:02:40 +0530

    Update Quiz.jsx

--- src/Pages/Teacher/Quiz.jsx
+++ src/Pages/Teacher/Quiz.jsx
@@ -1,11 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+ 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+[1:02 PM] Palak Gupta
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+ 
 const baseUrl = "https://danville.pythonanywhere.com/api";
-
+ 
 function Quiz(props) {
   const [status, setStatus] = useState(1); // Default status, you may need to fetch this from the server initially
-
+ 
   useEffect(() => {
     // Fetch the initial status from the server
     axios
@@ -17,7 +26,7 @@ function Quiz(props) {
         console.error(error);
       });
   }, [props.id]);
-
+ 
   const handleDisable = () => {
     // Example: Using your API endpoint and axios
     axios
@@ -40,31 +49,33 @@ function Quiz(props) {
         console.error(error);
       });
   };
-
+ 
   return (
-    <li className={`list-group-item ${status === 0 ? "disabled-quiz" : ""}`}>
-      <a href={props.link} target="_blank" rel="noreferrer">
-        {props.title}
-      </a>
-      <button
-        className="buttn btn-sm float-end text-white"
-        onClick={handleDisable}
-      >
-        {status === 0 ? "Enable" : "Disable"}
-      </button>
-    </li>
+    <div className="quizcontainer">
+      <li className={`list-group-item ${status === 0 ? "disabled-quiz" : ""}`}>
+        <a href={props.link} target="_blank" rel="noreferrer">
+          {props.title}
+        </a>
+        <button
+          className="buttn btn-sm float-end text-white"
+          onClick={handleDisable}
+        >
+          {status === 0 ? "Enable" : "Disable"}
+        </button>
+      </li>
+    </div>
   );
 }
-
+ 
 export default Quiz;
-
+ 
 // function Quiz(props) {
 //   return (
 //     <li className="list-group-item">
 //       <a href={props.link} target="_blank" rel="noreferrer">
 //         {props.title}
 //       </a>
-
+ 
 //       {/* <button
 //         className="DeleteVideo btn btn-sm float-end text-white"
 //         onClick={props.onDelete}
@@ -85,3 +96,4 @@ export default Quiz;
 //   );
 // }
 // export default Quiz;
+ 
