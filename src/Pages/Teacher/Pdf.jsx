9dbdaeb53678f62708743b0ce5bbd0b4aa328e19
Harshita Makode <96257348+harshita-2002-m@users.noreply.github.com>
2023-12-01 13:00:53 +0530

    Update Pdf.jsx

--- src/Pages/Teacher/Pdf.jsx
+++ src/Pages/Teacher/Pdf.jsx
@@ -1,20 +1,20 @@
 // Pdf.js
 import React from "react";
-
+ 
 function Pdf({ link, title, id }) {
   const openPdf = () => {
     window.open(link, "_blank");
   };
-
+ 
   return (
     <li className="list-group-item btn-flex">
       {title}
       <button className="text-white" href={link} target="_blank" rel="noopener noreferrer" onClick={openPdf}>
         View
-      
+     
       </button>
     </li>
   );
 }
-
+ 
 export default Pdf;
