b904776243f5d3effdf0036079a0b31c13c4f71e
Harshita Makode <96257348+harshita-2002-m@users.noreply.github.com>
2023-12-01 13:07:40 +0530

    Update ResetPassword.jsx

--- src/Pages/Teacher/ResetPassword.jsx
+++ src/Pages/Teacher/ResetPassword.jsx
@@ -1,64 +1,64 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
+ 
 const baseUrl = "https://danville.pythonanywhere.com/api";
-
+ 
 function ResetPassword() {
   const navigate = useNavigate();
   const [password, setPassword] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
   const [successMsg, setSuccessMsg] = useState("");
-
+ 
   const instructorId = localStorage.getItem("instructorId");
-
+ 
   const handleChange = (event) => {
     // Clear the error message when the user starts typing a new password
     setErrorMsg("");
     setPassword(event.target.value);
   };
-
+ 
   const submitForm = async () => {
     if (!password.trim()) {
       setErrorMsg("New password is required");
       return;
     }
-
+ 
     // Define the password regex pattern
     const passwordRegex =
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/;
-
+ 
     if (!passwordRegex.test(password)) {
       setErrorMsg(
         "Password must be at least 8 characters long and contain a mix of uppercase letters, lowercase letters, and numbers, and atleast one special character((?=.*[!@#$%^&*()_+{}[]:;<>,.?~\\/-]))"
       );
       return;
     }
-
+ 
     const formData = new FormData();
     formData.append("password", password);
-
+ 
     try {
       const response = await axios.post(
         baseUrl + `/instructor-reset-password/${instructorId}/`,
         formData
       );
-
+ 
       if (response.data.user_type) {
         setSuccessMsg("Password has been reset successfully");
       } else {
         setErrorMsg("Some error occurred while changing password");
       }
-
+ 
       // Wait for a few seconds before navigating
       setTimeout(() => {
-        navigate("/Login");
+        navigate("/");
       }, 2000); // Navigate after 3 seconds (adjust as needed)
     } catch (error) {
       console.log(error);
     }
   };
-
+ 
   return (
     <div className="card-body col-5 offset-5">
       <div className="login-container">
@@ -86,5 +86,5 @@ function ResetPassword() {
     </div>
   );
 }
-
+ 
 export default ResetPassword;
