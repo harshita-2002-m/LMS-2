a2e942d43254a5d1612bdae89ef55f24d6367124
Harshita Makode <96257348+harshita-2002-m@users.noreply.github.com>
2023-12-01 13:42:00 +0530

    Update Videos.jsx

--- src/Pages/Student/Videos.jsx
+++ src/Pages/Student/Videos.jsx
@@ -3,34 +3,34 @@ import Video from "./Video";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import VideoPlayer from "./Video";
-
+ 
 const baseUrl = "https://danville.pythonanywhere.com/api";
-
+ 
 function Videos() {
   const { id } = useParams();
   const [videosData, setVideosData] = useState([]);
   const [error, setError] = useState(null);
-
+ 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(`${baseUrl}/videos/`);
         const video = response.data.filter((video) => video.fk_course == id);
-
+ 
         setVideosData(video);
         console.log("PDF Content : ", video);
       } catch (error) {
         console.error(error);
       }
     };
-
+ 
     fetchData();
   }, [id]);
-
+ 
   if (error) {
     return <div>Error loading videos: {error.message}</div>;
   }
-
+ 
   return (
     <div className="container CourseSyllabus">
       <div className="syllabusFaq faqs text-light d-flex justify-content-between align-items-center">
@@ -49,5 +49,5 @@ function Videos() {
     </div>
   );
 }
-
+ 
 export default Videos;
